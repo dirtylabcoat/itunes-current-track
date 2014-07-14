7143aeb79cf461c9e90343e0aa8f78e8d3600c8c
Daniel Löfquist <daniel@lofquist.org>
2014-07-14 21:38:29 +0200

    Added copyright/license-notice to source

--- client/itunescurrent-client.sh
+++ client/itunescurrent-client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (c) 2011-2014 BSD-type License, see License.txt
 [[ "$1" = "" ]] && SERVERIP=192.168.1.108 || SERVERIP=$1
 [[ "$2" = "" ]] && SERVERPORT=4321 || SERVERPORT=$2
 CURRENTTRACK=""
--- server/itunescurrent-server.sh
+++ server/itunescurrent-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (c) 2011-2014 BSD-type License, see License.txt
 [[ "$1" = "" ]] && SERVERPORT=4321 || SERVERPORT=$1
 while [ 1 ]; do
 	osascript itunescurrent.scpt | nc -l $SERVERPORT
